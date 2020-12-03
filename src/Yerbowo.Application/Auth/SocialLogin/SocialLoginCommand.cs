f97df1b75990c3a0ca9de23beb18fd09e8aaaddf
Wojciech Ragan <wojteksc21@gmail.com>
2020-12-03 22:14:12 +0100

    Delete Id property from SocialLoginCommand

--- src/Yerbowo.Application/Auth/SocialLogin/SocialLoginCommand.cs
+++ src/Yerbowo.Application/Auth/SocialLogin/SocialLoginCommand.cs
@@ -4,7 +4,6 @@ namespace Yerbowo.Application.Auth.SocialLogin
 {
 	public class SocialLoginCommand : IRequest<ResponseToken>
 	{
-		public string Id { get; set; }
 		public string Email { get; set; }
 		public string FirstName { get; set; }
 		public string LastName { get; set; }
